df482e2d4d46b1aa0b862789bddc19b15113f8b9
XAVIER MAMANI CONDORI <xavier@MacBook-Pro-de-XAVIER.local>
2026-08-05 00:48:05 -0400

    add manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Condoriri 2026",
+        short_name: "Condoriri",
+        description: "Campeonato Condoriri 2026",
+        theme_color: "#1976d2",
+        background_color: "#ffffff",
+        display: "standalone",
+
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
